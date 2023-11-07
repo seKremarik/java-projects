e4b03c846456f48284dd66a58ee22c1d860ee2ca
Emilio Kremarik <emilio.kremarik@gmail.com>
2023-11-07 19:15:14 -0300

    Small change to main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import bot.SimpleBot;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         SimpleBot.greet("Bonita", "2023");
